74c3a3a0dcbe881b0c94c23d7ca4fbfbfd0d3263
Andrew Lapin <andrew.d.lapin@gmail.com>
2015-09-29 17:17:36 +0300

    Add locations model to main controller

--- frontend/src/app/index.module.js
+++ frontend/src/app/index.module.js
@@ -2,7 +2,9 @@ import config from './index.config';
 import routerConfig from './index.route';
 import MainController from './main/main.controller';
 
-angular.module('frontend', ['ngAnimate', 'restangular', 'ui.router', 'mm.foundation', 'yaMap'])
+import './components/ymaps/ymaps.module';
+
+angular.module('frontend', ['ngAnimate', 'restangular', 'ui.router', 'mm.foundation', 'ymaps'])
   .config(config)
   .config(routerConfig)
   .controller('MainController', MainController);
--- frontend/src/app/main/main.controller.js
+++ frontend/src/app/main/main.controller.js
@@ -1,8 +1,10 @@
+
 class MainController {
     constructor () {
         'ngInject';
 
         this.asd = 'world 1';
+        this.locations = ['location1', 'location2', 'location3', 'location4', 'location5'];
     }
 }
 
